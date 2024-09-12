6944009e43f891a49fb58a5622cc08a361436f13
wilson-developer-git <wilsonprogram@gmail.com>
2024-09-12 06:55:03 -0300

    Criado o header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,35 @@
+import imgLogo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+import { FiUser, FiLogIn } from 'react-icons/fi'
 
 export function Header() {
+  const signed = false;
+  const loadingAuth = false;
 
-    return (
-      <div>
-        <h1>Component header</h1>
-      </div>
-    )
-  }
-  
-  
+  return (
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
+        <Link to="/">
+          <img src={imgLogo} alt="Logo site" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="border-2 rounded-full p-2 border-gray-900">
+              <FiLogIn size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
+  )
+}
+
