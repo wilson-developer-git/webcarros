8467e9ea5be08fd8e8d618e83a6258b2d4acdda5
wilson-developer-git <wilsonprogram@gmail.com>
2024-09-13 06:55:02 -0300

    Criada pagina de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,74 @@
+import { Link } from "react-router-dom";
+import { Container } from "../../components/container";
+import logoImg from '../../assets/logo.svg'
+import { Input } from "../../components/input";
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  email: z.string().email("Insira um email válido!").nonempty("O campo email é obrigatório"),
+  password: z.string().nonempty("O campo senha é obrigatório")
+})
+
+type FormData = z.infer<typeof schema>
 
 export function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange"
+  })
 
-    return (
-      <div>
-        <h1>Pagina Login</h1>
-      </div>
-    )
+  function onSubmit(data: FormData) {
+    console.log(data)
   }
-  
-  
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link
+          className="mb-6 max-w-sm  w-full"
+          to="/">
+          <img
+            className="w-full"
+            src={logoImg}
+            alt="Logo" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-semibold"
+          >
+            Acessar
+          </button>
+          
+        </form>
+      </div>
+    </Container>
+  )
+}
+
