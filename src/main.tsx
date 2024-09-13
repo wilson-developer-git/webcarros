15be0b985c502a02ce3d3a63c8e43a158dd7d6f1
wilson-developer-git <wilsonprogram@gmail.com>
2024-09-13 20:45:15 -0300

    Criado o AuthContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './main.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './App'
+import AuthProvider from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
